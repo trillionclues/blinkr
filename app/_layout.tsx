281439a9c5c1dc506340c8d3143955819bb6290a
trillionclues <exceln646@gmail.com>
2024-01-18 21:45:06 +0100

    fix tabs nav

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,10 +9,7 @@ import { SplashScreen, Stack } from 'expo-router'
 import { useEffect } from 'react'
 import { useColorScheme } from 'react-native'
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router'
+export { ErrorBoundary } from 'expo-router'
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
